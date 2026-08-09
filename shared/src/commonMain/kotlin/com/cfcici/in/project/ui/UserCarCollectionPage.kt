acef3f53b5dc1c914970c5d8dd57edf411cc7494
Parvathi M <parvathim1504@gmail.com>
2026-08-10 03:49:18 +0530

    Updated ProfilePage.kt

--- shared/src/commonMain/kotlin/com/cfcici/in/project/ui/UserCarCollectionPage.kt
+++ shared/src/commonMain/kotlin/com/cfcici/in/project/ui/UserCarCollectionPage.kt
@@ -2,7 +2,9 @@ package com.cfcici.`in`.project.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -51,12 +54,16 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDefaults.color
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import com.cfcici.`in`.project.data.database.UserCar
-
+import login.shared.generated.resources.nissan_skyline
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserCarCollectionPage(UserCCPBrand: String, UserCCPUserId: Int){
@@ -119,12 +126,11 @@ fun UserCarCollectionPage(UserCCPBrand: String, UserCCPUserId: Int){
                     Image(
                     painter = painterResource(Res.drawable.wp12533988),
                     contentDescription = "Hotwheels",
-                    modifier = Modifier.fillMaxSize(),
-                        )
+                    modifier = Modifier.fillMaxSize(),)
                     IconButton(
-                    onClick = {
+                        onClick = {
 
-                    },
+                        },
                     modifier = Modifier
                         .align(Alignment.TopStart)
                         .padding(8.dp),
@@ -225,13 +231,24 @@ fun UserCarCollectionPage(UserCCPBrand: String, UserCCPUserId: Int){
                 // Add icon for filter *************************************************************
             }
         }
-            item {  }
+            item {
+                testCars.forEach {car ->
+                    EachCarTab(
+                        // because you use UserCar as datatype, you don't need to mention the car.model
+                        selectedCar =  car
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
-fun EachCarTab() {
+//if you are using : UserCar you are passing all the parameter in the UserCar instead of only one datatype
+fun EachCarTab(selectedCar: UserCar)
+{
+    val usernameFont = FontFamily(Font(Res.font.Amarante_Regular))
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -245,23 +262,74 @@ fun EachCarTab() {
             defaultElevation = 4.dp
         )
     ) {
-        // Put your car information here
+        Row( modifier = Modifier.padding(5.dp))
+        {
+            Column(modifier = Modifier.padding(vertical = 5.dp, horizontal = 25.dp)){
+                Image(
+                    painter = painterResource(Res.drawable.nissan_skyline),
+                    contentDescription = selectedCar.modelUser,
+                    modifier = Modifier
+                        .size(90.dp)
+                        .clip(RectangleShape),
+                    contentScale = ContentScale.Crop
+                )
+            }// need to add photo
+            
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            // even gap between every child no matter how many rows you have it will automatically space it equally
+            ) {
+                Text(
+                    text = selectedCar.modelUser,
+                    color = Color.White,
+                    fontFamily = usernameFont,
+                    fontSize = 20.sp
+                )
+                Row {
+                    // Because Text expect string you should convert to string
+                    Text(
+                        text = selectedCar.yearUser.toString(),
+                        color = Color.White,
+                        fontFamily = usernameFont,
+                        fontSize = 15.sp
+                    )
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = selectedCar.colourUser,
+                        color = Color.White,
+                        fontFamily = usernameFont,
+                        fontSize = 15.sp
+                    )
+                }
+                Row {
+                    // Because Text expect string you should convert to string
+                    Text(
+                        text = selectedCar.collectorNoUser.toString(),
+                        color = Color.White,
+                        fontFamily = usernameFont,
+                        fontSize = 15.sp
+                    )
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = selectedCar.seriesUser.toString(),
+                        color = Color.White,
+                        fontFamily = usernameFont,
+                        fontSize = 15.sp
+                    )
+                    Spacer(Modifier.width(30.dp))
+
+                    Text(
+                        text = selectedCar.typeOfSeriesUser.toString(),
+                        color = Color.White,
+                        fontFamily = usernameFont,
+                        fontSize = 15.sp
+                    )
+                }
+            }
+        }
     }
 }
-/*
-@Composable
-fun EachCarTab(){
-    Surface (
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp)
-            .height(100.dp),
-        shape = RoundedCornerShape(16.dp),
-        color = Color.DarkGray,
-        tonalElevation = 4.dp
-    ) {  }
-}*/
-
 @Preview(showBackground = true)
 @Composable
 fun UserCarCollectionPagePreview() {
